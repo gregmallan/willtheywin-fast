aaa9da761c6cfc7f54f6737ceb940d79d700277d
Greg Allan <greg.allan@ubc.ca>
2021-11-24 09:40:36 -0700

    Added Team ask endpoint

--- src/main.py
+++ src/main.py
@@ -33,6 +33,29 @@ async def create_team(team: TeamCreate, session: AsyncSession = Depends(get_sess
     return team
 
 
+@app.get('/teams/{team_id}/ask')
+async def team_will_they_win(team_id: int, sentiment: Optional[Sentiment] = None,
+                             session: AsyncSession = Depends(get_session)):
+
+    query = select(Team).where(Team.id == team_id)
+    results = await session.execute(query)
+    team = results.first()
+
+    if team is None:
+        return {'OK': False, 'team': None, 'error': f'No matching team for id={team_id}'}
+
+    answer_choices_callable = AnswerChoices.any
+    if sentiment == Sentiment.NEGATIVE:
+        answer_choices_callable = AnswerChoices.negative
+    elif sentiment == Sentiment.NEUTRAL:
+        answer_choices_callable = AnswerChoices.neutral
+    elif sentiment == Sentiment.POSITIVE:
+        answer_choices_callable = AnswerChoices.positive
+
+    answer = answer_choices_callable()
+    return {'team': team, 'answer': answer, 'requested_sentiment': sentiment}
+
+
 @app.get('/ask/{team}')
 async def ask(team: str, sentiment: Optional[Sentiment] = None):
     answer_choices_callable = AnswerChoices.any
