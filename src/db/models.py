eedc2ed329f51541e689ab21e192be13089d663f
Greg Allan <greg.allan@ubc.ca>
2021-11-24 11:18:43 -0700

    Adding indexes to Team model and regenerate initial mig

--- src/db/models.py
+++ src/db/models.py
@@ -1,17 +1,21 @@
 from typing import Optional
 
-from sqlmodel import SQLModel, Field
+from sqlalchemy import Column, String, UniqueConstraint
+from sqlmodel import SQLModel, Field, Index
 
 
 class TeamBase(SQLModel):
-    name: str
+    name: str = Field(sa_column=Column('name', String, nullable=False, index=True, unique=False))
     city: str
     sport: str
 
 
 class Team(TeamBase, table=True):
-    id: int = Field(default=None, primary_key=True)
-    # name = Column(String, index=True)  # TODO: How to add index
+    __table_args__ = (
+        UniqueConstraint('name', 'city', 'sport', name='team_name_city_sport_unique_idx'),
+    )
+
+    id: int = Field(default=None, primary_key=True, nullable=False)
 
 
 class TeamCreate(TeamBase):
