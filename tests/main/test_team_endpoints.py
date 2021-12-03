a4fc5ee844525e7b429c7b76a7fd2b82b2d60559
Greg Allan <greg.allan@ubc.ca>
2021-12-03 09:48:28 -0700

    Test get_teams when no teams; move existing get_teams test to class

--- tests/main/test_team_endpoints.py
+++ tests/main/test_team_endpoints.py
@@ -52,19 +52,26 @@ def sort_team_dicts_by_id(teams: List[Dict]):
 
 
 @pytest.mark.asyncio
-async def test_get_teams(async_client, teams):
-    response = await async_client.get('/teams')
-    assert response.status_code == 200
-    assert response.json()
+class TestGetTeams:
 
-    response_teams = response.json()
-    sort_team_dicts_by_id(response_teams)
-    sort_team_objs_by_id(teams)
+    async def test_get_teams_none_exist(self, async_client, db):
+        response = await async_client.get('/teams')
+        assert response.status_code == 200
+        assert response.json() == []
 
-    # TODO: Why does pytest say list of dicts == list of Team objects. Not using only this assertion until I know why.
-    #  I think it may come from SQLModel <- pydantic BaseModel <- ModelMetaclass. See test_model_eq.py
-    assert response.json() == teams
+    async def test_get_teams(self, async_client, teams):
+        response = await async_client.get('/teams')
+        assert response.status_code == 200
+        response_teams = response.json()
+        assert response_teams
 
-    for team_dict, team in zip(response_teams, teams):
-        assert team_dict == team
-        assert team_dict == team.dict()
+        sort_team_dicts_by_id(response_teams)
+        sort_team_objs_by_id(teams)
+
+        # TODO: Why does pytest say list of dicts == list of Team objects. Not using only this assertion until I know why.
+        #  I think it may come from SQLModel <- pydantic BaseModel <- ModelMetaclass. See test_model_eq.py
+        assert response.json() == teams
+
+        for team_dict, team in zip(response_teams, teams):
+            assert team_dict == team
+            assert team_dict == team.dict()
